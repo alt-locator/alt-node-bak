e15bc266f19cee2d4edf2ffcc04f30c349dca6ef
cnishina <craig.nishina@gmail.com>
2016-07-24 16:09:27 -0700

    chore(server): response status and logging

--- src/server/host.server.ts
+++ src/server/host.server.ts
@@ -1,32 +1,52 @@
+import {Logger} from 'top-banana-logger';
 import {Config} from '../config';
 import {Location, NodeModel} from '../node/location';
-import {AltFirebaseService, NodeObject} from './firebase.service';
+import {AltFirebaseService, NodeObject} from './alt.service';
 import * as http from 'http';
 import * as express from 'express';
 import * as expressCore from 'express-serve-static-core';
 
+let logger = new Logger('host.server');
 let app = express();
+
+app.get('/',
+    (request: expressCore.Request,
+    response: expressCore.Response) => {
+
+  // get the list of hosts from firebase
+  AltFirebaseService.getHosts().then(hosts => {
+    logger.debug('GET /', 200);
+    response.send(hosts);
+  }).catch(err => {
+    logger.error('GET /', 500, err);
+    response.sendStatus(500);
+  });
+});
 
 /**
  * The public API endpoint. Will make patch requests to other local ip addresses
  * that have matching external addresses.
  */
-app.get('/',
+app.get('/update',
     (request: expressCore.Request,
     response: expressCore.Response) => {
 
   // update the current machine
   AltFirebaseService.updateHost(Config.locationName).then(status => {
     if (!status) {
+      logger.debug('"GET /update"', 400);
       response.sendStatus(400);
     } else {
-
+      logger.debug('"GET /update"', 200);
       // get the list of hosts
       // the list of hosts
       // go through and make requests to other local servers
       updateAllLocal();
       response.send('done!');
     }
+  }).catch(err => {
+    logger.error('"GET /update"', 500, err)
+    response.sendStatus(500);
   });
 });
 
@@ -72,10 +92,14 @@ app.get('/patch',
   console.log('PATCH');
   AltFirebaseService.updateHost(Config.locationName).then((result: boolean) => {
     if (result) {
+      logger.debug('"GET /patch"', 200);
       response.send('updated');
     } else {
+      logger.debug('"GET /patch"', 400);
       response.send('not updated');
     }
+  }).catch(err => {
+    logger.debug('"GET /patch"', 500, err);
   });
 });
 
