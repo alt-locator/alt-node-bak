0dfee08d5ab879dfaa3b6a9beaf81f22e7bb7fba
cnishina <craig.nishina@gmail.com>
2016-07-27 23:43:02 -0700

    chore(config): use os hostname over config hard coded

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,11 @@
+import * as os from 'os';
 import {Logger, LogLevel} from 'top-banana-logger';
 import {NodeModel, Location} from './node/location';
 Logger.logLevel = LogLevel.DEBUG;
 
 export let Config = {
   firebaseHost: 'alternative-4ae3a.firebaseio.com',
-  locationName: 'hp-laptop'
+  locationName: os.hostname()
 }
 
 export let CurrNode = new NodeModel(Config.locationName);
