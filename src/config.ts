6c938bfa980f35083d48a8b6367ee5ee373092f7
Craig Nishina <craig.nishina@gmail.com>
2016-08-04 23:49:22 -0700

    chore(travis): add a travis config to run an update

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@ import {NodeModel, Location} from './node/location';
 Logger.logLevel = LogLevel.DEBUG;
 
 export let Config = {
-  firebaseHost: 'alt-github.firebaseio.com',
-  locationName: os.hostname()
+  firebaseHost: process.env['ALT_HOST'] || 'alt-github.firebaseio.com',
+  locationName: process.env['ALT_NAME'] || os.hostname()
 }
 
 export let CurrNode = new NodeModel(Config.locationName);
