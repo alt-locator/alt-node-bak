9068bb14bf15ac7c33dc21c419c1c7f061e1f3ba
Craig <cnishina@users.noreply.github.com>
2017-02-07 00:32:54 -0800

    chore(config): provide a config interface (#28)
    - split apart configuration interfaces so they are in logical pieces
    - main configuration interface extends other configs.

---
+++ lib/config/firebase_config.ts
@@ -0,0 +1,21 @@
+/**
+ * The firebase storage configuration.
+ */
+export interface FirebaseConfig {
+  /**
+   * The firebase host name. For example this could be: 'foo-baz.firebaseio.com'. The value
+   * of the firebaseHost can be overriden by environment variable 'ALT_FIREBASE_HOST'.
+   * 
+   * The default value is 'alt-github.firebaseio.com'.
+   */
+  firebaseHost?: string;
+
+  /**
+   * The initial path to save everything. So for example, if the path is '/foobaz', the items
+   * stored would be at 'foo-baz.firebase.io/foobaz'. The value of the firebasePath can be
+   * overriden by the environment variable 'ALT_FIREBASE_PATH'.
+   * 
+   * The default value is '/hosts'.
+   */
+  firebasePath?: string;
+}
---
+++ lib/config/location_config.ts
@@ -0,0 +1,12 @@
+/**
+ * The configurations based on the location of this machine.
+ */
+export interface LocationConfig {
+  /**
+   * The location name of this machine (required). The value of the locationName can be overriden
+   * by the environment variable 'ALT_NAME'.
+   * 
+   * The default value is this machine's hostname.
+   */
+  locationName: string;
+}
